f629b0f0493068b6772b50d8088459716f5fab48
wahyuindra <wahyuindra712@gmail.com>
2022-05-08 23:30:17 +0700

    add configure register activity

--- app/src/main/java/org/brainless/telyucreative/views/authscreen/RegisterActivity.kt
+++ app/src/main/java/org/brainless/telyucreative/views/authscreen/RegisterActivity.kt
@@ -1,8 +1,11 @@
 package org.brainless.telyucreative.views.authscreen
 
-import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.auth.AuthResult
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 import org.brainless.telyucreative.R
 import org.brainless.telyucreative.databinding.ActivityRegisterBinding
 
@@ -15,8 +18,71 @@ class RegisterActivity : BaseActivity() {
         binding = ActivityRegisterBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        binding.btnRegister.setOnClickListener{
+            registerUser()
+        }
+
         binding.tvLoginOptionClick.setOnClickListener{
             onBackPressed()
+        }
+    }
+
+    private fun validateRegisterDetails(): Boolean {
+        return when {
+            TextUtils.isEmpty(binding.edtFirstName.text.toString().trim { it <= ' ' }) -> {
+                showErrorSnackBar(resources.getString(R.string.err_msg_enter_first_name), true)
+                false
+            }
+
+            TextUtils.isEmpty(binding.edtLastName.text.toString().trim { it <= ' ' }) -> {
+                showErrorSnackBar(resources.getString(R.string.err_msg_enter_last_name), true)
+                false
+            }
+
+            TextUtils.isEmpty(binding.edtEmail.text.toString().trim { it <= ' ' }) -> {
+                showErrorSnackBar(resources.getString(R.string.err_msg_enter_email), true)
+                false
+            }
+
+            TextUtils.isEmpty(binding.edtPassword.text.toString().trim { it <= ' ' }) -> {
+                showErrorSnackBar(resources.getString(R.string.err_msg_enter_password), true)
+                false
+            }
+            else -> {
+                true
+            }
+        }
+    }
+
+    private fun registerUser() {
+
+        if (validateRegisterDetails()) {
+
+            showProgressDialog(resources.getString(R.string.please_wait))
+
+            val email: String = binding.edtEmail.text.toString().trim { it <= ' ' }
+            val password: String = binding.edtPassword.text.toString().trim { it <= ' ' }
+
+            FirebaseAuth.getInstance().createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener { task ->
+
+                    hideProgressDialog()
+
+                    if (task.isSuccessful) {
+
+                        val firebaseUser: FirebaseUser = task.result!!.user!!
+
+                        showErrorSnackBar(
+                            "You are registered successfully. Your user id is ${firebaseUser.uid}",
+                            false
+                        )
+                        FirebaseAuth.getInstance().signOut()
+                        finish()
+                    } else {
+                        // If the registering is not successful then show error message.
+                        showErrorSnackBar(task.exception!!.message.toString(), true)
+                    }
+                }
         }
     }
 }
