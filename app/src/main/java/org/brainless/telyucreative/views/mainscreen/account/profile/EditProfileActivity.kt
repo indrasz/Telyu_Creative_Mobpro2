9228fb3b7210e307d9f49dbc46cbd51503857243
wahyuindra <wahyuindra712@gmail.com>
2022-05-14 00:28:12 +0700

    update edit profile page

--- app/src/main/java/org/brainless/telyucreative/views/mainscreen/account/profile/EditProfileActivity.kt
+++ app/src/main/java/org/brainless/telyucreative/views/mainscreen/account/profile/EditProfileActivity.kt
@@ -3,10 +3,17 @@ package org.brainless.telyucreative.views.mainscreen.account.profile
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import org.brainless.telyucreative.R
+import org.brainless.telyucreative.databinding.ActivityEditProfileBinding
 
 class EditProfileActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityEditProfileBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_edit_profile)
+        binding = ActivityEditProfileBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        supportActionBar?.hide()
+
     }
 }
