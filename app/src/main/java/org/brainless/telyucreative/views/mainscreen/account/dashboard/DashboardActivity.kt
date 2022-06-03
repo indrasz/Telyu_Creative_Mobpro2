bfddaa9b243815714b7639d3e899a34c1d26fedf
wahyuindra <wahyuindra712@gmail.com>
2022-06-03 16:28:42 +0700

    update ui dashboard

--- app/src/main/java/org/brainless/telyucreative/views/mainscreen/account/dashboard/DashboardActivity.kt
+++ app/src/main/java/org/brainless/telyucreative/views/mainscreen/account/dashboard/DashboardActivity.kt
@@ -3,10 +3,16 @@ package org.brainless.telyucreative.views.mainscreen.account.dashboard
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import org.brainless.telyucreative.R
+import org.brainless.telyucreative.databinding.ActivityDashboardBinding
 
 class DashboardActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityDashboardBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_dashboard)
+        binding = ActivityDashboardBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
+
+
 }
